811de38f47c26c85baf7dd436066a30a2f29aa3a
Luis Paulo M. Degini <luispaulo.degini@gmail.com>
2020-03-31 21:59:27 -0300

    Validação das rotas e tests

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+const { errors } = require('celebrate')
 const routes = require('./routes')
 
 const app = express()
@@ -9,6 +10,6 @@ app.use(morgan('tiny'))
 app.use(cors())
 app.use(express.json())
 app.use(routes)
+app.use(errors())
 
-app.listen(3333)
-console.log('Servidor online: 3333')
+module.exports = app
